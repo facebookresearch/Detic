0e75f56cd1223db820f1df3995f75ef0f5faba1a
Bea Steers <bea.steers@gmail.com>
2023-09-05 19:16:26 -0400

    allow default predictor to take arbitrary args

--- detic/predictor.py
+++ detic/predictor.py
@@ -7,7 +7,7 @@ import cv2
 import torch
 
 from detectron2.data import MetadataCatalog
-from detectron2.engine.defaults import DefaultPredictor
+from detectron2.engine.defaults import DefaultPredictor as _DefaultPredictor
 from detectron2.utils.video_visualizer import VideoVisualizer
 from detectron2.utils.visualizer import ColorMode, Visualizer
 
@@ -160,6 +160,25 @@ class VisualizationDemo(object):
         else:
             for frame in frame_gen:
                 yield process_predictions(frame, self.predictor(frame))
+
+
+
+
+class DefaultPredictor(_DefaultPredictor):
+    def _preprocess(self, original_image):
+        if self.input_format == "RGB":
+            original_image = original_image[:, :, ::-1]
+        height, width = original_image.shape[:2]
+        image = self.aug.get_transform(original_image).apply_image(original_image)
+        image = torch.as_tensor(image.astype("float32").transpose(2, 0, 1))
+        return image, height, width
+
+    def __call__(self, original_image, *a, **kw):
+        with torch.no_grad():
+            image, height, width = self._preprocess(original_image)
+            predictions = self.model([{"image": image, "height": height, "width": width}], *a, **kw)[0]
+            return predictions
+
 
 
 class AsyncPredictor:
